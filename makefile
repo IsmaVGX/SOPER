4c2e632cb01e0706e89d6c2952f0906925322421
IsmaVGX <ismael.bartolome@estudiante.uam.es>
2026-02-26 15:12:38 +0100

    he empezado el b, no esta ni mucho menos funcional

---
+++ makefile
@@ -0,0 +1,21 @@
+CC = gcc
+CFLAGS = -Wall -g
+TARGET = Minero
+OBJS = miner.o pow.o
+
+all: $(TARGET)
+
+run: $(TARGET)
+	./$(TARGET) 10004 6 5
+ 
+$(TARGET): $(OBJS)
+	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
+
+miner.o: miner.c pow.h
+	$(CC) $(CFLAGS) -c $< -o $@
+
+pow.o: pow.c pow.h
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(TARGET) *.o
--- miner.c
+++ miner.c
@@ -2,17 +2,25 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
+#include "pow.h"
 
 int main(int argc, char *argv[])
 {
 
-    int i;
+    // int i;
     pid_t pid;
     int n_hilos;
     int n_rounds;
     pid_t registrador;
     int target;
 
+    /*Comprobacion para que pete al tener un numero incorrecto de argumentos*/
+    if (argc != 4)
+    {
+        printf("Error en los argumentos\n");
+        return -1;
+    }
 
     target = atoi(argv[1]);
     n_rounds = atoi(argv[2]);
@@ -27,27 +35,41 @@ int main(int argc, char *argv[])
     }
     else if (pid == 0)
     {
-        printf("Registrador %d\n", i);
+        printf("Registrador %d\n", getpid());
         exit(EXIT_SUCCESS);
     }
     else if (pid > 0)
     {
         printf("Soy el minero. Rondas: %d, Hilos: %d\n", n_rounds, n_hilos);
 
+        target = pow_hash(target);
+
+        pthread_t threads[n_hilos];
+
+        for (int i = 0; i < n_hilos; i++)
+        {
+            pthread_create(&threads[i], NULL, funcion_hilo, NULL); //meTER AQUI una funcion, supongo
+        }
+
+        for (int i = 0; i < n_hilos; i++)
+        {
+            pthread_join(threads[i], NULL);
+        }
+
         int status;
         waitpid(pid, &status, 0);
 
         if (WIFEXITED(status))
         {
-            printf("exit con status %d\n",WEXITSTATUS(status));
-        }else{
+            printf("exit con status %d\n", WEXITSTATUS(status));
+        }
+        else
+        {
             printf("exit unexpected\n");
         }
-        
     }
 
     wait(NULL);
     printf("minero exit con status 0\n");
     exit(EXIT_SUCCESS);
-
 }
